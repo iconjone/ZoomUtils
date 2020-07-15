e1f3eff1fd86559c00619b27f25a8ed390575fd8
JoshuaDerickSamuel <JoshuaDerickSamuel@gmail.com>
2020-07-14 23:22:06 -0500

    Vuetify support

--- src/options/options.js
+++ src/options/options.js
@@ -1,10 +1,13 @@
 import Vue from 'vue';
 import App from './App';
+import Vuetify from "vuetify";
 
+   Vue.use(Vuetify);
 global.browser = require('webextension-polyfill');
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+    vuetify: new Vuetify(),
   render: h => h(App),
 });
--- src/popup/popup.js
+++ src/popup/popup.js
@@ -2,6 +2,9 @@ import Vue from 'vue';
 import App from './App';
 import store from '../store';
 import router from './router';
+import Vuetify from "vuetify";
+
+   Vue.use(Vuetify);
 
 global.browser = require('webextension-polyfill');
 Vue.prototype.$browser = global.browser;
@@ -11,5 +14,6 @@ new Vue({
   el: '#app',
   store,
   router,
+  vuetify: new Vuetify(),
   render: h => h(App),
 });
