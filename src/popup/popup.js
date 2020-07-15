8b09a4889ff729fbfaeeebf1706b0acdcb5aa441
iconjone <jone.bala@gmail.com>
2020-07-15 00:31:12 -0500

    Theming and setup

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -2,9 +2,27 @@ import Vue from 'vue';
 import App from './App';
 import store from '../store';
 import router from './router';
-import Vuetify from "vuetify";
+import Vuetify from 'vuetify';
 
-   Vue.use(Vuetify);
+Vue.use(Vuetify);
+const vuetify = new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: '#4dacfe',
+        secondary: '#002f57',
+        accent: '#38f527',
+        error: '#b71c1c',
+      },
+      dark: {
+        primary: '#002f57',
+        secondary: '#4dacfe',
+        accent: '#38f527',
+        error: '#b71c1c',
+      },
+    },
+  },
+});
 
 global.browser = require('webextension-polyfill');
 Vue.prototype.$browser = global.browser;
@@ -14,6 +32,6 @@ new Vue({
   el: '#app',
   store,
   router,
-  vuetify: new Vuetify(),
+  vuetify: vuetify,
   render: h => h(App),
 });
--- src/popup/router/pages/Index.vue
+++ src/popup/router/pages/Index.vue
@@ -1,9 +1,6 @@
 <template>
-<v-app id="inspire">
-    <v-navigation-drawer
-      v-model="drawer"
-      app
-    >
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer" app>
       <v-list dense>
         <v-list-item link>
           <v-list-item-action>
@@ -24,34 +21,18 @@
       </v-list>
     </v-navigation-drawer>
 
-    <v-app-bar
-      app
-      color="indigo"
-      dark
-    >
+    <v-app-bar app>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-      <v-toolbar-title>Application</v-toolbar-title>
+      <v-toolbar-title>Zoom Utils</v-toolbar-title>
     </v-app-bar>
 
     <v-main>
-      <v-container
-        class="fill-height"
-        fluid
-      >
-        <v-row
-          align="center"
-          justify="center"
-        >
+      <v-container class="fill-height" fluid>
+        <v-row align="center" justify="center">
           <v-col class="text-center">
             <v-tooltip left>
               <template v-slot:activator="{ on }">
-                <v-btn
-                  :href="source"
-                  icon
-                  large
-                  target="_blank"
-                  v-on="on"
-                >
+                <v-btn :href="source" icon large target="_blank" v-on="on">
                   <v-icon large>mdi-code-tags</v-icon>
                 </v-btn>
               </template>
@@ -61,11 +42,8 @@
         </v-row>
       </v-container>
     </v-main>
-    <v-footer
-      color="indigo"
-      app
-    >
-      <span class="white--text">&copy; {{ new Date().getFullYear() }}</span>
+    <v-footer app>
+      <span class="">&copy; {{ new Date().getFullYear() }} Jonathan Samuel</span>
     </v-footer>
   </v-app>
 </template>
@@ -73,7 +51,7 @@
 <script>
 export default {
   data() {
-    return {};
+    return { drawer: null };
   },
 };
 </script>
